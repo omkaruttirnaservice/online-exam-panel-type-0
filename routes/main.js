6631063d649697e112d0c1cccee5071b9a3e430d
omkaruttirnaservice <omkaruttirnaservice@gmail.com>
2025-06-05 10:16:38 +0530

    feat: new api-check for checking connection with server from kiosk app

--- routes/main.js
+++ routes/main.js
@@ -1,69 +1,52 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-var commenMiddleware = require("./middleware");
-var indexController = require("../application/controllers/indexConroller");
+var commenMiddleware = require('./middleware');
+var indexController = require('../application/controllers/indexConroller');
+
+router.get('/api-check', (req, res, next) => {
+    return res.status(200).json({
+        call: 1,
+        message: 'Server connected.',
+    });
+});
 
 /* GET home page. */
-router.get(
-  "/",
-  commenMiddleware.checkForPoolConnection,
-  indexController.getExamHomePage
+router.get('/', commenMiddleware.checkForPoolConnection, indexController.getExamHomePage);
+router.get('/student-add', commenMiddleware.checkForPoolConnection, indexController.addNewStudent);
+router.post(
+    '/add-new-student',
+    commenMiddleware.checkForPoolConnection,
+    indexController.postNewStudent
 );
+router.post('/chk/link', commenMiddleware.checkForPoolConnection, indexController.checkExamLink);
 router.get(
-  "/student-add",
-  commenMiddleware.checkForPoolConnection,
-  indexController.addNewStudent
+    '/add-student',
+    commenMiddleware.checkForPoolConnectionWithSession,
+    indexController.addStudentView
 );
 router.post(
-  "/add-new-student",
-  commenMiddleware.checkForPoolConnection,
-  indexController.postNewStudent
-);
-router.post(
-  "/chk/link",
-  commenMiddleware.checkForPoolConnection,
-  indexController.checkExamLink
+    '/verify-candidate',
+    commenMiddleware.checkForPoolConnection,
+    indexController.verifyCandidateForExam
 );
 router.get(
-  "/add-student",
-  commenMiddleware.checkForPoolConnectionWithSession,
-  indexController.addStudentView
+    '/exam-aggrement',
+    commenMiddleware.checkForPoolConnection,
+    indexController.viewExamAggrement
 );
-router.post(
-  "/verify-candidate",
-  commenMiddleware.checkForPoolConnection,
-  indexController.verifyCandidateForExam
+router.get('/confirmtion', commenMiddleware.checkForPoolConnection, indexController.Confirmtion);
+router.get('/warm-up', commenMiddleware.checkForPoolConnection, indexController.viewWarmUp);
+router.get('/test', commenMiddleware.checkForPoolConnection, indexController.startExamination);
+router.post('/go', indexController.updateTestDetails);
+router.get(
+    '/climax/:publish_id/:student_id',
+    commenMiddleware.checkForPoolConnection,
+    indexController.viewExamClimax
 );
 router.get(
-  "/exam-aggrement",
-  commenMiddleware.checkForPoolConnection,
-  indexController.viewExamAggrement
-);
-router.get(
-  "/confirmtion",
-  commenMiddleware.checkForPoolConnection,
-  indexController.Confirmtion
-);
-router.get(
-  "/warm-up",
-  commenMiddleware.checkForPoolConnection,
-  indexController.viewWarmUp
-);
-router.get(
-  "/test",
-  commenMiddleware.checkForPoolConnection,
-  indexController.startExamination
-);
-router.post("/go", indexController.updateTestDetails);
-router.get(
-  "/climax/:publish_id/:student_id",
-  commenMiddleware.checkForPoolConnection,
-  indexController.viewExamClimax
-);
-router.get(
-  "/testisdone",
-  commenMiddleware.checkForPoolConnection,
-  indexController.getExamIsDonePage
+    '/testisdone',
+    commenMiddleware.checkForPoolConnection,
+    indexController.getExamIsDonePage
 );
 
 module.exports = router;

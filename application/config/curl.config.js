8c3b484135335657ed5c5b9c4a3625579a9baad1
omkaruttirnaservice <omkaruttirnaservice@gmail.com>
2025-09-29 14:20:26 +0530

    updated: urls

--- application/config/curl.config.js
+++ application/config/curl.config.js
@@ -1,10 +1,10 @@
 var _ip_adress = require('../../ip_data.js')
 var host = _ip_adress.host
 var CURL_link = {
-    new_exam_list: host + 'getNewExamList',
-    download_exam: host + 'DownloadExam',
-    download_student_batch: host + 'DownloadStudentBatch',
-    upload_exam_link: host + 'saveUploadedExam',
-    get_center_data: host + 'getCenterData',
+    new_exam_list: host + '/gov/getNewExamList',
+    download_exam: host + '/gov/DownloadExam',
+    download_student_batch: host + '/gov/DownloadStudentBatch',
+    upload_exam_link: host + '/gov/saveUploadedExam',
+    get_center_data: host + '/gov/getCenterData',
 };
 exports.CURL_link = CURL_link;
